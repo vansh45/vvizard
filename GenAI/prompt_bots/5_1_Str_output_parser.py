6658deb737752ca75d605ac86eecb1b50a97c690
vansh45 <vanshsamaiya@gmail.com>
2025-11-25 16:36:40 -0500

    Used str output parser today

---
+++ GenAI/prompt_bots/5_1_Str_output_parser.py
@@ -0,0 +1,34 @@
+# Working with LLMs that donot support structured outputs by using output parsers that can do this job
+# Output Parsers in LangChain help convert raw LLM responses into structured formats like JSON, CSV, Pydantic models, and more. They ensure consistency, validation, and ease of use in applications.
+
+#String output parser
+ 
+from langchain_openai import ChatOpenAI # will work with any type of API
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+
+model = ChatOpenAI()
+
+# 1st prompt -> detailed report
+template1 = PromptTemplate(
+    template='Write a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+# 2nd prompt -> summary  Without it: Error! Template gets an object, not a string
+template2 = PromptTemplate(
+    template='Write a 5 line summary on the following text. /n {text}',
+    input_variables=['text']
+)
+
+parser = StrOutputParser()
+
+chain = template1 | model | parser | template2 | model | parser
+
+result = chain.invoke({'topic':'black hole'})
+
+print(result)
