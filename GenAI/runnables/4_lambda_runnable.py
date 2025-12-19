4c1c73d8042a83b8e51f1205182b918349d2a7a5
vansh45 <vanshsamaiya@gmail.com>
2025-12-19 13:09:06 -0500

    used lambda and branch runnable

--- GenAI/runnables/4_lambda_runnable.py
+++ GenAI/runnables/4_lambda_runnable.py
@@ -2,11 +2,14 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-from langchain.schema.runnable import RunnableSequence, RunnableParallel, RunnablePassthrough
+from langchain_core import RunnableSequence, RunnableLambda, RunnablePassthrough, RunnableParallel
 
 load_dotenv()
 
-prompt1 = PromptTemplate(
+def word_count(text):
+    return len(text.split())
+
+prompt = PromptTemplate(
     template='Write a joke about {topic}',
     input_variables=['topic']
 )
@@ -15,18 +18,17 @@ model = ChatOpenAI()
 
 parser = StrOutputParser()
 
-prompt2 = PromptTemplate(
-    template='Explain the following joke - {text}',
-    input_variables=['text']
-)
-
-joke_gen_chain = RunnableSequence(prompt1, model, parser)
+joke_gen_chain = RunnableSequence(prompt, model, parser)
 
 parallel_chain = RunnableParallel({
     'joke': RunnablePassthrough(),
-    'explanation': RunnableSequence(prompt2, model, parser)
+    'word_count': RunnableLambda(word_count)
 })
 
 final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
 
-print(final_chain.invoke({'topic':'cricket'}))
+result = final_chain.invoke({'topic':'AI'})
+
+final_result = """{} \n word count - {}""".format(result['joke'], result['word_count'])
+
+print(final_result)
